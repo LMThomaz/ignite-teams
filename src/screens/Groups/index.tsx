64b716538cbec0a651439b0504c697962897c773
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-05-25 08:24:23 -0300

    listing groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,25 @@
 import { GroupCard } from '@components/GroupCard'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 import * as S from './styles'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    'Ignite React Native',
+    'Turma 2022',
+  ])
+
   return (
     <S.Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-      <GroupCard title="Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </S.Container>
   )
 }
