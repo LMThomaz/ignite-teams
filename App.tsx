002bbdb88dd52ad972bb1c995fdcec15600e4075
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-05-24 23:35:03 -0300

    define theme for app

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
 import { Groups } from '@screens/Groups'
+import { ThemeProvider } from 'styled-components'
+import theme from './src/theme'
 
 export default function App() {
-  return <Groups />
+  return (
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
+  )
 }
