1bf706be5e1c0763bebea7bdf2e88796d43c024b
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-05-25 08:29:33 -0300

    component ListEmpty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,13 @@
 import { GroupCard } from '@components/GroupCard'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
+import { ListEmpty } from '@components/ListEmpty'
 import { useState } from 'react'
 import { FlatList } from 'react-native'
 import * as S from './styles'
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>([
-    'Ignite React Native',
-    'Turma 2022',
-  ])
+  const [groups, setGroups] = useState<string[]>([])
 
   return (
     <S.Container>
@@ -19,6 +17,10 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
     </S.Container>
   )
