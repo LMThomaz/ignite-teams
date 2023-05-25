5782529693bd9c8793a29c8e5cd7db59faa1722d
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-05-25 07:55:57 -0300

    styling statusBar

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import {
   useFonts,
 } from '@expo-google-fonts/roboto'
 import { Groups } from '@screens/Groups'
+import { StatusBar } from 'react-native'
 import { ThemeProvider } from 'styled-components'
 import theme from './src/theme'
 
@@ -13,6 +14,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   )
