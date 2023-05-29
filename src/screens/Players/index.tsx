89ee557c6e2ab8f9923841b86d9c50629f5902f3
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-05-29 17:10:12 -0300

    finally list players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,8 +1,10 @@
+import { Button } from '@components/Button'
 import { ButtonIcon } from '@components/ButtonIcon'
 import { Filter } from '@components/Filter'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Input } from '@components/Input'
+import { ListEmpty } from '@components/ListEmpty'
 import { PlayerCard } from '@components/PlayerCard'
 import { useState } from 'react'
 import { FlatList } from 'react-native'
@@ -44,7 +46,18 @@ export function Players() {
         renderItem={({ item }) => (
           <PlayerCard name={item} onRemove={() => {}} />
         )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time." />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          {
+            paddingBottom: 100,
+          },
+          players.length === 0 && { flex: 1 },
+        ]}
       />
+      <Button title="Remover turma" type="SECONDARY" />
     </S.Container>
   )
 }
