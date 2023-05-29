7404972e112b805f9ad48a1f3b7009d7f6d69820
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-05-29 16:52:10 -0300

    listing filters

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,9 +3,14 @@ import { Filter } from '@components/Filter'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Input } from '@components/Input'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 import * as S from './styles'
 
 export function Players() {
+  const [team, setTeam] = useState('Time A')
+  const [players, setPlayers] = useState(['Thomaz'])
+
   return (
     <S.Container>
       <Header showBackButton />
@@ -17,7 +22,21 @@ export function Players() {
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </S.Form>
-      <Filter title="Time A" isActive />
+      <S.HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={team === item}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <S.NumbersOfPlayers>{players.length}</S.NumbersOfPlayers>
+      </S.HeaderList>
     </S.Container>
   )
 }
