dcf372d9bafe6fbcf76aa78dd1d16103f25807af
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-05-31 15:17:55 -0300

    navigate in application

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,12 +3,19 @@ import { GroupCard } from '@components/GroupCard'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { ListEmpty } from '@components/ListEmpty'
+import { useNavigation } from '@react-navigation/native'
 import { useState } from 'react'
 import { FlatList } from 'react-native'
 import * as S from './styles'
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
+
+  const navigation = useNavigation()
+
+  function handleNewGroup() {
+    navigation.navigate('new')
+  }
 
   return (
     <S.Container>
