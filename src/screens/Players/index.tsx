e8da3c8bb9255d6daabbb0ef6022e7a0177ad312
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-06-01 07:46:14 -0300

    fetch player by group and team

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -7,8 +7,9 @@ import { Input } from '@components/Input'
 import { ListEmpty } from '@components/ListEmpty'
 import { PlayerCard } from '@components/PlayerCard'
 import { useRoute } from '@react-navigation/native'
+import { PlayerStorageDTO } from '@storage/player/PlayerStorageDTO'
 import { playerAddByGroup } from '@storage/player/playerAddByGroup'
-import { playersGetByGroup } from '@storage/player/playersGetByGroup'
+import { playersGetByGroupAndTeam } from '@storage/player/playersGetByGroupAndTeam'
 import { AppError } from '@utils/AppError'
 import { useState } from 'react'
 import { Alert, FlatList } from 'react-native'
@@ -21,7 +22,7 @@ type RouteParams = {
 export function Players() {
   const [newPlayerName, setNewPlayerName] = useState('')
   const [team, setTeam] = useState('Time A')
-  const [players, setPlayers] = useState(['Thomaz'])
+  const [players, setPlayers] = useState<PlayerStorageDTO[]>([])
 
   const route = useRoute()
   const { group } = route.params as RouteParams
@@ -39,8 +40,6 @@ export function Players() {
 
     try {
       await playerAddByGroup(newPlayer, group)
-      const players = await playersGetByGroup(group)
-      console.log(players)
     } catch (error) {
       if (error instanceof AppError) {
         Alert.alert('Nova pessoa', error.message)
@@ -48,6 +47,24 @@ export function Players() {
       }
       console.log(error)
       Alert.alert('Nova pessoa', 'Não foi possível adicionar')
+    }
+  }
+
+  async function fetchPlayersByTeam() {
+    try {
+      const playersByTeam = await playersGetByGroupAndTeam(group, team)
+      setPlayers(playersByTeam)
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert('Nova pessoa', error.message)
+        return
+      }
+      console.log(error)
+
+      Alert.alert(
+        'Nova pessoa',
+        'Não foi possível carregas as pessoas do time selecionado',
+      )
     }
   }
 
