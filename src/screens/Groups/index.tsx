f1283a50a2d3fc6151aedbf5dbd1a14eef84a555
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-06-01 07:11:48 -0300

    open groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -27,6 +27,10 @@ export function Groups() {
     }
   }
 
+  function handleOpenGroup(group: string) {
+    navigation.navigate('players', { group })
+  }
+
   useFocusEffect(
     useCallback(() => {
       fetchGroups()
@@ -40,7 +44,9 @@ export function Groups() {
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
+        renderItem={({ item }) => (
+          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
+        )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
